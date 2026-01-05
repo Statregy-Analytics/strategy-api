3e96ccbbbaebaff965845e4fc68ed47d0df2bb99
JorgeNunes <jlbnunes@live.com>
2026-01-05 19:17:17 -0300

    [FIX]  name field investment personal one year in export

--- app/Exports/WalletExport.php
+++ app/Exports/WalletExport.php
@@ -31,7 +31,7 @@ class WalletExport implements FromCollection, WithHeadings, WithMapping
             'Patrimonial no último mês',
             'Reserva de emergência',
             'Emergência no último mês',
-            'Personalizado 1 ano',
+            'Investimento Personalizado 1 ano',
             'Personalizado 1 ano no último mês',
         ];
     }
